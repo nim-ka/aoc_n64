707e7719a2453a79fd9fce2711373950753469df
strager <strager.nds@gmail.com>
2018-08-01 07:46:57 -0700

    Fix size of cosine lookup table (#209)

--- data/math_util.data.s
+++ data/math_util.data.s
@@ -8,7 +8,10 @@ glabel gSineTable
 	.incbin "bin/sm64.j.101998.bin", 0x70, 0x1000
 
 glabel gCosineTable
-	.incbin "bin/sm64.j.101998.bin", 0x1070, 0x4000
+	.incbin "bin/sm64.j.101998.bin", 0x1070, 0x1000
+
+glabel D_80388000
+	.incbin "bin/sm64.j.101998.bin", 0x2070, 0x3000
 
 glabel D_8038B000
 	.incbin "bin/sm64.j.101998.bin", 0x5070, 0x810
