2f9613a55aaae2e71754abea6efb3059c84f7826
tehzz <tehzz@users.noreply.github.com>
2019-09-01 11:06:44 -0400

    Add tidy.sh file (#678)
    * fix libultra header missing includes
    
    * add clang-tidy
    
    * rename tidy script and move to tools

---
+++ tools/clang-tidy.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+shopt -s globstar
+
+# Use clang-tidy to brace if statements and loops. This should be a feature of clang-format.
+# The brace check is the only check being applied right now, 
+# but other checks (like naming scheme) may be added at a later time. 
+
+# '--fix-errors` due to generated text_strings.h as well as the enhancement inc.c files
+TIDY_OPTS="-p . --fix --fix-errors" 
+COMPILER_OPTS="-nostdinc -fno-builtin -std=gnu90 -Iinclude -Isrc -D_LANGUAGE_C"
+VERSIONS=("-DVERSION_US" "-DVERSION_JP" "-DVERSION_EU")
+
+if (( $# > 0 )); then
+    printf "Tidy file(s) $*"
+    echo
+    for VER in "${VERSIONS[@]}"; do  
+        echo "with compiler version flag ${VER}"
+        clang-tidy ${TIDY_OPTS} "$@" -- ${COMPILER_OPTS} ${VER}
+    done
+    printf "Done tidying file(s) $*"
+    echo
+    exit
+fi
+
+echo "Tidying all C files for all versions. This will take a bit"
+for VER in "${VERSIONS[@]}"; do  
+        echo "Tidying for compiler version flag ${VER}"
+        clang-tidy ${TIDY_OPTS} src/**/*.c -- ${COMPILER_OPTS} ${VER}
+        clang-tidy ${TIDY_OPTS} lib/src/*.c -- ${COMPILER_OPTS} ${VER}
+        clang-tidy ${TIDY_OPTS} enhancements/*.inc.c -- ${COMPILER_OPTS} ${VER}
+done
+echo "Done tidying all C files. Hope you did something fun during the tidying"
